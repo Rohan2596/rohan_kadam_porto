86b4a7ea2e481c2bf08adb1e90614cea3e5a5879
Rohan2596 <rohankadam662@gmail.com>
2021-08-12 17:54:08 +0530

    WIP:About us Component in progress.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './component/header/HeaderComponent';
 import HomeComponent from './component/body/home';
+import AboutComponent from './component/body/about';
 
 function App() {
   return (
     <div className="App">
-      <HeaderComponent/>
+      <HeaderComponent />
       <HomeComponent/>
+      <AboutComponent/>
     </div>
   );
 }
